5c14c9a809fd30c0205bbd5313705f2054cad002
Jedd Dryden <40693089+Jaffa-Cakes@users.noreply.github.com>
2023-07-10 18:34:42 +1000

    checkpoint

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build --tag rust-build ./rust-build/
-docker build --tag rust-final ./rust-final/
-docker build --tag rust-final-db ./rust-final-db/
+docker build --tag rust-build ./rust-build/ #--platform linux/arm64
+docker build --tag rust-final ./rust-final/ #--platform linux/arm64
+docker build --tag rust-final-db ./rust-final-db/ #--platform linux/arm64
